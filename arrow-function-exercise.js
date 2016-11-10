c3c37d5e7e7b2fa0497f25b8bfea46825cccd9a9
Krister <krister.torsvik@gmail.com>
2016-11-10 09:20:23 +0100

    arrow function exercise

---
+++ arrow-function-exercise.js
@@ -0,0 +1,21 @@
+'use strict';
+'format es6'; // force SystemJS to transpile exercise
+
+class MyDiv {
+  constructor(selector) {
+    this.element = document.getElementById(selector);
+    this.words = ['JavaScript', 'For', 'Life'];
+  }
+
+  write() {
+    this.words.forEach((word) => {
+      this.element.innerHTML += word + ' ';
+    });
+  }
+}
+
+var example = new MyDiv('example');
+example.write();
+
+//just took refactored from this.words.forEach(function (word)
+// to the result
